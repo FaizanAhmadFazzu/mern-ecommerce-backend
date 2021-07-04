aaacc921e4279f082a9014f002b39f3fc041c560
Faizan Ahmad <faizan@opskube.com>
2021-07-04 18:43:26 +0530

    validatation signup signin data

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+
 exports.signup = (req, res) => {
     User.findOne({ email: req.body.email})
     .exec((error, user) => {
