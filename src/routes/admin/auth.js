69d90511ff0bb9bf7c6bcb23d5dbdecad7c40f45
Faizan Ahmad <faizan@opskube.com>
2021-07-18 15:06:47 +0530

    fixed issues

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const { signup, signin, signout } = require("../../controllers/admin/auth");
 const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require("../../validators/auth");
-const { requireSignin } = require('../../common-middleware/index')
 const router = express.Router()
 
 
 
 router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin)
-router.post('/admin/signout', requireSignin, signout)
+router.post('/admin/signout', signout)
 
 
 
