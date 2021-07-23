72904b4ebdf5be8ee26edd7ae3ac6f0732e4b05b
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-07-23 20:42:27 +0530

    improvement product api

--- src/controllers/admin/auth.js
+++ src/controllers/admin/auth.js
@@ -1,7 +1,8 @@
 const User = require('../../models/user')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
     User.findOne({ email: req.body.email })
         .exec((error, user) => {
             if (user) return res.status(400).json({
@@ -15,11 +16,12 @@ exports.signup = (req, res) => {
         email,
         password
     } = req.body;
+    const hash_password = await bcrypt.hash(password, 10)
     const _user = new User({
         firstName,
         lastName,
         email,
-        password,
+        hash_password,
         username: Math.random().toString(),
         role: 'admin'
     });
