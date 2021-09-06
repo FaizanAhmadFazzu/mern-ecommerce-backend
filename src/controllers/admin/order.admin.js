1c547049e16b448234cf23727ceab8d011e6ba8b
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-09-06 15:13:40 +0530

    order Details API

--- src/controllers/admin/order.admin.js
+++ src/controllers/admin/order.admin.js
@@ -12,6 +12,13 @@ exports.updateOrder = (req, res) => {
     }
   ).exec((error, order) => {
     if (error) return res.status(400).json({ error });
-    if (order) return res.status(200).json({ order });
+    if (order) return res.status(201).json({ order });
   });
+};
+
+exports.getCustomerOrders = async (req, res) => {
+  const orders = await Order.find({})
+    .populate("items.productId", "name")
+    .exec();
+  res.status(200).json({ orders });
 };
